033353c3cf98c0e3f432e31667ecb0b247a717e0
maxrichardburgess <22722054+maxfire2008@users.noreply.github.com>
2021-10-15 19:05:57 +1100

    Update problem_io.py

--- problem_io.py
+++ problem_io.py
@@ -1,7 +1,9 @@
+import sys
+import base64
 class IO:
     @property
     def input(self):
-        return "2"
+        return base64.b64decode(sys.argv[1].encode()).decode()
     def output(self,output):
-        print("PROBLEM_IO:output")
+        print(base64.b64encode(str(output).encode()).decode())
 io = IO()
