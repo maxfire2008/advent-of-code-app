36c9871419bba6d9e81e5cf4430bf117a4c2f495
maxrichardburgess <22722054+maxfire2008@users.noreply.github.com>
2021-10-15 19:28:03 +1100

    Update problem_io.py

--- problem_io.py
+++ problem_io.py
@@ -5,5 +5,5 @@ class IO:
     def input(self):
         return base64.b64decode(sys.argv[1].encode()).decode()
     def output(self,output):
-        print(base64.b64encode(str(output).encode()).decode())
+        print('__AOC_CI_SYSTEM_OUTPUT_CALL:'+base64.b64encode(str(output).encode()).decode())
 io = IO()
