b9e13f29a7e5f4307f3b11e7b77a14d8d9ea01b6
maxrichardburgess <22722054+maxfire2008@users.noreply.github.com>
2021-10-15 17:57:06 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 import tkinter as tk
+import tkinter.simpledialog
 import datetime
+import requests
+import os
+
+_AOC_TOKEN=None
+
+def aoc_token():
+    global _AOC_TOKEN
+    if _AOC_TOKEN:
+        return _AOC_TOKEN
+    else:
+        _AOC_TOKEN = tkinter.simpledialog.askstring("AoC Token","Advent of Code Token")
+        return _AOC_TOKEN
+
+_PROBLEMS_DIR = None
+
+def problems_dir():
+    global _PROBLEMS_DIR
+    if _PROBLEMS_DIR:
+        return _PROBLEMS_DIR
+    else:
+        _PROBLEMS_DIR = tkinter.simpledialog.askstring("Problems Directory","Problems Directory")
+        return _PROBLEMS_DIR
 
 def current_time():
     return datetime.datetime.utcnow()-datetime.timedelta(hours=5)#+datetime.timedelta(days=50)
 
 win = tk.Tk()
 win.title("AoC CI")
-win.geometry("200x650")
+win.geometry("1000x650")
 win.resizable(0, 0)
+
+win.rowconfigure(1,weight=3)
+win.columnconfigure(1,weight=2)
 
 year_list_widget = tk.Listbox(win, exportselection=False)
 day_list_widget = tk.Listbox(win, exportselection=False)
@@ -28,11 +54,14 @@ def year_select_action(event):
     if selection:
         index = selection[0]
         data = event.widget.get(index)
-        print(data)
+##        print(data)
         day_list_widget.delete(0,tk.END)
         for day in range(len(day_list[int(data)])):
             day_list_widget.insert(day,str(day_list[int(data)][day]))
 ##        day_list_widget.select_set(0)
+        saveall()
+        sample_input.delete(1.0,tk.END)
+        sample_output.delete(1.0,tk.END)
     else:
         print("none selected")
 def day_select_action(event):
@@ -40,7 +69,10 @@ def day_select_action(event):
     if selection:
         index = selection[0]
         data = event.widget.get(index)
-        print(data)
+##        print(data)
+        saveall()
+        sample_input.delete(1.0,tk.END)
+        sample_output.delete(1.0,tk.END)
     else:
         print("none selected")
 year_list_widget.bind('<<ListboxSelect>>', year_select_action)
@@ -48,9 +80,49 @@ year_list_widget.bind('<<ListboxSelect>>', year_select_action)
 day_list_widget.bind('<<ListboxSelect>>', day_select_action)
 
 year_list_widget.grid(column=0,row=0)
-day_list_widget.grid(column=0,row=1, columnspan=2)
+day_list_widget.grid(column=0,row=1,stick="nesw")
+
+sample_input_frame = tk.Frame(win)
+sample_input_frame.grid(column=1,row=0,rowspan=2)
+
+sample_input_label = tk.Label(sample_input_frame, text="Sample Input")
+sample_input_label.pack()
+sample_input = tk.Text(sample_input_frame,height=12,width=40)
+sample_input.pack()
+##def save_sample_input():
+##    text = sample_input.get(1.0,tk.END)
+##    print("save_sample_input")
+##sample_input_button = tk.Button(sample_input_frame, text="Save & Apply", command=save_sample_input)
+##sample_input_button.pack()
+
+sample_input_label = tk.Label(sample_input_frame, text="Sample Output")
+sample_input_label.pack()
+sample_output = tk.Text(sample_input_frame,height=12,width=40)
+sample_output.pack()
+##def save_sample_output():
+##    text = sample_output.get(1.0,tk.END)
+##    print("save_sample_output")
+##sample_output_button = tk.Button(sample_input_frame, text="Save & Apply", command=save_sample_output)
+##sample_output_button.pack()
 
 ##year_list_widget.select_set(tk.END)
 ##year_list_widget.event_generate("<<ListboxSelect>>")
+
+def saveall():
+    print("Saving")
+    day_list_selection = day_list_widget.curselection()
+    if day_list_selection:
+        day_list_index = day_list_selection[0]
+        day_list_data = day_list_widget.get(day_list_index)
+        year_list_selection = year_list_widget.curselection()
+        if year_list_selection:
+            year_list_index = year_list_selection[0]
+            year_list_data = year_list_widget.get(year_list_index)
+            print(os.path.join(problems_dir(),year_list_data,day_list_data))
+def on_closing():
+    saveall()
+    win.destroy()
+
+win.protocol("WM_DELETE_WINDOW", on_closing)
 
 win.mainloop()
