ea10d1641a403ffdbca6cc1ae2cf36798cbfe97b
maxrichardburgess <22722054+maxfire2008@users.noreply.github.com>
2021-10-16 13:49:21 +1100

    Update problem_io.py

--- problem_io.py
+++ problem_io.py
@@ -3,7 +3,13 @@ import base64
 class IO:
     @property
     def input(self):
-        return base64.b64decode(sys.argv[1].encode()).decode()
-    def output(self,output):
-        print('__AOC_CI_SYSTEM_OUTPUT_CALL:'+base64.b64encode(str(output).encode()).decode())
+        file_name = base64.b64decode(sys.argv[1].encode()).decode()
+        with open(file_name,"rb") as file:
+            file_content = file.read().decode()
+        return file_content
+    def output(self,output,part=None):
+        if "2" in str(part):
+            print('__AOC_CI_SYSTEM_OUTPUT_CALL_2:'+base64.b64encode(str(output).encode()).decode())
+        else:
+            print('__AOC_CI_SYSTEM_OUTPUT_CALL:'+base64.b64encode(str(output).encode()).decode())
 io = IO()
