e5998a7ed1d8ca79afad54c1b4d493101244742b
maxrichardburgess <22722054+maxfire2008@users.noreply.github.com>
2021-10-18 12:32:18 +1100

    Update problem_io.py

--- problem_io.py
+++ problem_io.py
@@ -6,7 +6,10 @@ class IO:
         file_name = base64.b64decode(sys.argv[1].encode()).decode()
         with open(file_name,"rb") as file:
             file_content = file.read().decode()
-        return file_content
+        if file_content.endswith("\n"):
+            return file_content[:-1]
+        else:
+            return file_content
     def output(self,output,part=None):
         if "2" in str(part):
             print('__AOC_CI_SYSTEM_OUTPUT_CALL_2:'+base64.b64encode(str(output).encode()).decode())
